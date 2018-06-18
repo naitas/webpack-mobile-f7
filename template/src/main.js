038bb0262fa310af5bc27b45025a5dcff7b2054b
MINHO <naitas@naver.com>
2018-06-18 19:23:02 +0900

    fr7 boilplate적용

--- template/src/main.js
+++ template/src/main.js
@@ -1,49 +1,42 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import Vuex from 'vuex'
+// Import Vue
+import Vue from 'vue';
 
 // Import F7
-/* eslint-disable no-unused-vars */
-import Framework7 from 'framework7'
+import Framework7 from 'framework7/dist/framework7.esm.bundle.js';
 
 // Import F7 Vue Plugin
-import Framework7Vue from 'framework7-vue'
+import Framework7Vue from 'framework7-vue/dist/framework7-vue.esm.bundle.js';
 
-// Import F7 iOS Theme Styles
-/* eslint-disable no-unused-vars */
-import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
-import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
-/* OR for Material Theme:
-import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
-import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
-*/
+// Import F7 Styles
+import Framework7Styles from 'framework7/dist/css/framework7.css';
 
-// Import App Custom Styles
-// import AppStyles from './css/app.css'
+// Import Icons and App Custom Styles
+import IconsStyles from './css/icons.css';
+import AppStyles from './css/app.css';
 
 // Import Routes
 import Routes from './routes.js'
 
-import App from './App'
+// Import App Component
+import App from './app';
 
 // Init F7 Vue Plugin
-Vue.use(Framework7Vue)
+Vue.use(Framework7Vue, Framework7)
 
-/* eslint-disable no-new */
 // Init App
 new Vue({
   el: '#app',
   template: '<app/>',
   // Init Framework7 by passing parameters here
   framework7: {
-    root: '#app',
-    /* Uncomment to enable Material theme: */
-    // material: true,
-    routes: Routes
+    id: 'io.framework7.testapp', // App bundle ID
+    name: 'Framework7', // App name
+    theme: 'auto', // Automatic theme detection
+    // App routes
+    routes: Routes,
   },
   // Register App Component
   components: {
     app: App
   }
-})
+});
